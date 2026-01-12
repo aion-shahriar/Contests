5a0e2a6ec471f94f65a2a4b9bd7fccd40d690cca
aion-shahriar <aionshahriar23@gmail.com>
2026-01-12 18:54:40 +0600

    Add TWOPLG.cpp for CodeChef problem solution
    Implement solution for TWOPLG problem on CodeChef.

---
+++ CodeChef/STARTERS_220/TWOPLG.cpp
@@ -0,0 +1,17 @@
+// Link: https://www.codechef.com/problems/TWOPLG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    while(t--) {
+        int x,y; cin>>x>>y;
+        
+        if(x%2==0) cout<<"Bob\n";
+        else cout<<"Alice\n";
+        
+    }
+    return 0;
+
+}
