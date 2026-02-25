b5aaac373b51cf97109f2ce33b3dfa767287dbc4
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 23:18:08 +0600

    Implement string check for specific characters
    Check if the input string matches specific characters at certain positions.

---
+++ CodeChef/STARTERS_227/FONTA.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/START227D/problems/FONTA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	string s; cin>>s;
+	if(s[4]=='a'-0 && s[3]=='t'-0 && s[2]=='n'-0) cout<<"YES\n";
+	else cout<<"NO\n";
+	
+	return 0;
+
+}
